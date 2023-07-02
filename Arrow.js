0634a9e7c517251d3d501c46cd76ca0a083071dd
monotusniks <nikmkare@gmail>
2023-07-02 15:22:52 +0530

    Arrow function

---
+++ Arrow.js
@@ -0,0 +1,12 @@
+// HTML file associated to it is - event_index.html
+ 
+//normal function
+function abc(){
+    console.log("hello there")
+}
+abc()
+ 
+// using Arrow function (=>)
+ 
+const efg = ()=> console.log("arrow function")
+efg()
